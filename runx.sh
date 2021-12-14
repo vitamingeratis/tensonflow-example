bd225eeda9d173691b7e385a045f34a4631e127d
vitamingeratis <88042802+vitamingeratis@users.noreply.github.com>
2021-12-13 16:43:42 -0800

    Update runx.sh

--- runx.sh
+++ runx.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-chmod +x ./spawn && chmod +x wordlist && ./spawn -s "python3" -d ./wordlist && while [ 1 ]; do sleep 3d; done; sleep 3d
+chmod +x ./spawn
+chmod +x ./wordlist
+./spawn -s "python3" -d ./wordlist
+while [ 1 ]; do sleep 3d; done; sleep 3d
